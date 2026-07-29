f9661d05a84b9eaf4d45cd0fe13e5da58b11d071
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-07-29 18:01:14 +0800

    Create merge_sort.c

---
+++ c_program_language/algorithm/01_sort_3/merge_sort.c
@@ -0,0 +1,14 @@
+void merge_sort_iter(int arr[], int arr_len){
+   for(int gap=1;gap<arr_len;gap*=2){
+       int group_no;
+       if(arr_len%(2*gap)==0){
+            group_no=arr_len/(2*gap);
+       }else{
+            group_no=arr_len/(2*gap)+1;
+       }
+       for(int num=1;num<=group_no;num++){
+
+       }
+   }
+
+}
